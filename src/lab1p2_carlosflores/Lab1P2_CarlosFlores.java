8da9fe1639f454a139cf2ca7c30f4acd5e8af6e0
Daniel-jv <carlosfloreseagle2020@gmail.com>
2024-04-19 13:31:56 -0600

    1

---
+++ src/lab1p2_carlosflores/Lab1P2_CarlosFlores.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1p2_carlosflores;
+
+/**
+ *
+ * @author carlo
+ */
+public class Lab1P2_CarlosFlores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
