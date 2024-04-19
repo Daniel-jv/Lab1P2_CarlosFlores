4b84ea0755a637604c81b2b8c203c09b5241b57b
Daniel-jv <carlosfloreseagle2020@gmail.com>
2024-04-19 13:43:09 -0600

    Ya hice lo mas facil :) 2

--- src/lab1p2_carlosflores/Lab1P2_CarlosFlores.java
+++ src/lab1p2_carlosflores/Lab1P2_CarlosFlores.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab1p2_carlosflores;
 
-/**
- *
- * @author carlo
- */
+import java.util.Scanner;
+import java.util.Random;
+
 public class Lab1P2_CarlosFlores {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Random rand = new Random();
+    static Scanner leer = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.print("Ingrese el tamaño de la matriz: ");
+        int tam = leer.nextInt();
+        while(tam % 2 == 0 || tam < 5){
+            System.out.print("ERROR, ingrese el tamaño: ");
+            tam = leer.nextInt();
+        }
+        int [][] mat = create(tam);
+        print(mat);
+    }
+    
+    public static int [][] create(int tam){
+        int [][] mat = new int [tam][tam];
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                int data = rand.nextInt(9-0)+1-0;
+                mat[i][j] = data;
+            }
+        }
+        return mat;
+    }
+    
+    public static void print(int [][] mat){
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print("["+mat[i][j]+"]");
+            }
+            System.out.println();
+        }
     }
     
 }
